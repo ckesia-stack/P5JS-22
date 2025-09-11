f780c792c150cd59607cff9b320f4d863aa0e425
Kesia Vaz <c.kesia@escola.pr.gov.br>
2025-09-11 10:44:10 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,55 +6,31 @@ let xJogador1 = 0;
 let xJogador2 = 0;
 
 function draw() {
-  ativaJogo()
-  desenhaJogadores()
-  desenhaLinhaDeChegada()
-  verificarVencedor()
-}
-function ativaJogo(){
-  if(focused == true){
-    background(220);
+  if (focused == true) {
+    background("#D2EBB5");
   } else {
-    background("rgb(61,248,61)")
+    background("rgb(238,178,178)");
   }
-}
-
-function desenhaJogadores() {
-  textSize(40)
-  text("🥰", xJogador1, 100)
-  text("🤩", xJogador2, 300)
-}
-function desenhaLinhaDeChegada(){
-  fill("white")
-  rect(350, 0, 10, 400)
-  fill("black")
-  for(let yAtual = 0; yAtual<400; yAtual+=20) { rect(350,yAtual, 10, 10)
-  }
-}
-
- function verificarVencedor() { 
- if(xJogador>350) {
- text("Jogador 1 venceu!", 20, 200)
- noLoop();
-     }
-if (xJogador2 > 350) {
-text("Jogador 2 venceu!", 20, 200);
-noLoop();
-}  
-}
-
-function keyRealeased(){
-  if(key == "a"){
-  xJogador1 += random(20);
-  }
-  if(key == "s"){
-  xJogador2 += random(20);  
-  }
-}
-  
-  
+  textSize(40);
+  text("😎", xJogador1, 100);
+  text("❤️", xJogador2, 300);
+  rect(350, 0, 10, 400);
 
   if (xJogador1 > 350) {
-    text("Jogador 1 venceu!", 20, 200);
+    text("Jogador 1 venceu!", 50, 200);
     noLoop();
   }
+  if (xJogador2 > 350) {
+    text("Jogador 2 venceu!", 50, 200);
+    noLoop();
+  }
+}
+
+function keyReleased() {
+  if (key === "a") {
+    xJogador1 += random(20);
+  }
+  if (key === "s") {
+    xJogador2 += random(20);
+  }
+}
